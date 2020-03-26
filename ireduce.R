805ce1189971769e3b6d90006629ddc5629668dc
griipen <62502299+griipen@users.noreply.github.com>
2020-03-26 11:24:24 +0100

    Add files via upload

--- ireduce.R
+++ ireduce.R
@@ -1,3 +1,5 @@
+#Reduce-derivative which receives f and x, and applies f(x) on its output until x == f(x) or maxiter is reached:
+
 ireduce = function(f, x, maxiter = 50){
   i = 1
   while(!identical(f(x), x) & i <= maxiter) {x = f(x); i = i+1}; x
